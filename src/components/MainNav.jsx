8a992e839a4b34c837780262572e4e07090f0457
Suhali Preetham <43031804+Suhali-98@users.noreply.github.com>
2021-08-18 23:07:03 +0530

    updated MainNav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,16 +2,16 @@ import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-
+import TvIcon from "@material-ui/icons/Tv";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import TheatersIcon from "@material-ui/icons/Theaters";
-import TvIcon from "@material-ui/icons/Tv";
+
 import SearchIcon from "@material-ui/icons/Search";
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
-    width: 500,
+    width: "100%",
     position: "fixed",
     bottom: 0,
     backgroundColor: "#252525",
@@ -51,7 +51,7 @@ function SimpleBottomNavigation() {
       />
       <BottomNavigationAction
         style={{ color: "white" }}
-        label="Series"
+        label="TV Series"
         icon={<TvIcon />}
       />
       <BottomNavigationAction
